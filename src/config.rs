46ffe3037d005ef8f6d3be2b4f297b7050a25190
Jack Wickham <jackwickham@live.co.uk>
2025-12-30 23:05:29 +0000

    Allow configuring secrets file path

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use config::{Config, ConfigError, File};
 use duration_str::deserialize_duration_time;
 use serde::{Deserialize, Serialize};
@@ -8,15 +10,12 @@ use url::Url;
 pub struct ForcefieldConfig {
     pub public_root: Url,
     pub root_domain: String,
-
-    pub secret_key: String,
+    pub enable_hash_password: bool,
 
     #[serde(deserialize_with = "deserialize_duration_time")]
     pub login_cookie_expiration: Duration,
 
-    #[serde(default)]
-    pub enable_hash_password: bool,
-
+    pub secret_key: String,
     pub users: Vec<ConfigUser>,
 }
 
@@ -28,11 +27,13 @@ pub struct ConfigUser {
 
 impl ForcefieldConfig {
     pub fn load() -> Result<Self, ConfigError> {
+        let secrets_file_path = env::var("FORCEFIELD_SECRETS_FILE")
+            .unwrap_or_else(|_| "forcefield-secrets.toml".into());
         let s = Config::builder()
             .set_default("login_cookie_expiration", "7d")?
             .set_default("enable_hash_password", false)?
             .add_source(File::with_name("forcefield.toml"))
-            .add_source(File::with_name("forcefield-secrets.toml").required(false))
+            .add_source(File::with_name(&secrets_file_path).required(false))
             .build()?;
 
         s.try_deserialize()
