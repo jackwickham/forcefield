c6b9e87144f1c68f16b1839987c9bddc39cb0125
Jack Wickham <jackwickham@live.co.uk>
2025-12-30 18:33:21 +0000

    Static server and security headers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 use axum::{
-    Router, middleware,
+    Router,
+    body::Body,
+    extract::Request,
+    http::{
+        HeaderValue, Response,
+        header::{
+            CONTENT_SECURITY_POLICY, REFERRER_POLICY, STRICT_TRANSPORT_SECURITY, X_XSS_PROTECTION,
+        },
+    },
+    middleware::{self, Next},
     routing::{get, post},
 };
 use axum_extra::extract::cookie::Key;
 use base64::Engine;
 use time::Duration;
+use tower_http::services::ServeDir;
 use url::Url;
 
 use crate::{
@@ -41,13 +51,15 @@ pub async fn start_server(config: Config) -> Result<(), std::io::Error> {
         .route("/", get(index_handler))
         .route("/check-auth", get(check_auth))
         .route("/login", get(show_login).post(login))
-        .route("/logout", get(logout));
+        .route("/logout", get(logout))
+        .nest_service("/static", ServeDir::new("static"));
     if config.enable_hash_password {
         app_builder = app_builder.route("/hash-password", post(hash_password));
     }
 
     let app = app_builder
         .layer(middleware::from_fn(auto_cookie_middleware))
+        .layer(middleware::from_fn(response_headers_middleware))
         .with_state(initial_state(config));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000")
@@ -55,6 +67,32 @@ pub async fn start_server(config: Config) -> Result<(), std::io::Error> {
         .expect("Failed to start listening on 0.0.0.0:8000");
     println!("Listening on 0.0.0.0:8000");
     axum::serve(listener, app).await
+}
+
+async fn response_headers_middleware(req: Request, next: Next) -> Response<Body> {
+    let mut res = next.run(req).await;
+    let headers = res.headers_mut();
+    headers.insert(
+        CONTENT_SECURITY_POLICY,
+        HeaderValue::from_static(concat!(
+            "default-src 'self'; ",
+            "script-src 'none'; ",
+            "object-src 'none'; ",
+            "base-uri 'none'; ",
+            "frame-ancestors 'none'; ",
+        )),
+    );
+    headers.insert(X_XSS_PROTECTION, HeaderValue::from_static("0"));
+    headers.insert(
+        STRICT_TRANSPORT_SECURITY,
+        HeaderValue::from_static("max-age=31104000; includeSubDomains"), // 1y
+    );
+    headers.insert(
+        REFERRER_POLICY,
+        HeaderValue::from_static("strict-origin-when-cross-origin"),
+    );
+
+    res
 }
 
 fn initial_state(config: Config) -> ForcefieldState {
